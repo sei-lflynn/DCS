034901d8c103e830889b502d3a71f48779bcce77
JP Pan (393C) <james.paul.j.pan@jpl.nasa.gov>
2025-01-31 10:30:16 -0800

    fixing dev sa-gui setup after rebase

--- kmc-sa-mgmt/kmc-sa-gui/src/main/frontend/src/setupProxy.js
+++ kmc-sa-mgmt/kmc-sa-gui/src/main/frontend/src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:8447',
+            target: 'http://localhost:8447/sa-mgmt',
             changeOrigin: true,
         })
     );
